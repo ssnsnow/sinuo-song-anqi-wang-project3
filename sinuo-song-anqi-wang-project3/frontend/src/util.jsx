5dee93d2d64ed951d3c807c23bee01d4fb6de171
songsin <song.sin@northeastern.edu>
2023-04-26 22:38:08 -0700

    delete not used folder

--- sinuo-song-anqi-wang-project3/frontend/src/util.jsx
+++
@@ -1,8 +0,0 @@
-export function formatDate(date) {
-  const datetoParse = new Date(date);
-  const year = datetoParse.getFullYear();
-  const month = (datetoParse.getMonth() + 1).toString().padStart(2, "0");
-  const day = datetoParse.getDate().toString().padStart(2, "0");
-  const formattedDate = `${year}-${month}-${day}`;
-  return formattedDate;
-}
